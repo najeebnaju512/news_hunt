edc01af605ed7b63d36b4e1c1a4b399bbc1f0471
najeebnaju512 <najeebnaju512@gmail.com>
2024-04-21 00:30:08 +0530

    last edit

--- lib/controller/search_screenCntrl.dart
+++ lib/controller/search_screenCntrl.dart
@@ -5,14 +5,14 @@ import 'package:news_now/model/news_model.dart';
 import 'package:http/http.dart' as http;
 
 class SearchSreenController with ChangeNotifier {
-   NewsDataModel? newsDataModel;
+  NewsDataModel? newsDataModel;
   bool isLoading = false;
 
   Future<void> searchData({required String searchText}) async {
     isLoading = true;
     notifyListeners();
     final url = Uri.parse(
-        "https://newsapi.org/v2/everything?q=$searchText&from=2024-03-06&sortBy=popularity&apiKey=233c06f8ee1b4faf9aba34353b5e73c7");
+        "https://newsapi.org/v2/everything?q=$searchText&from=2024-04-19&to=2024-04-19&sortBy=popularity&apiKey=233c06f8ee1b4faf9aba34353b5e73c7");
     try {
       final response =
           await http.get(url, headers: {'Content-type': 'application/json'});
--- lib/view/widget/newsCard.dart
+++ lib/view/widget/newsCard.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:provider/provider.dart';
-import '../../controller/home_screeCntrl.dart';
 
 class NewsCard extends StatelessWidget {
   const NewsCard(
